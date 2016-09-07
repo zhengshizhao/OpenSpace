679cbff29ac3ca2c0d0b9965bb7cd2779c43def8
Jonathas Costa <jon.costa@gmail.com>
2016-09-07 16:58:37 -0400

    Changed dynamic root scene graph functions declarations for performance improved ones.

--- include/openspace/util/setscene.h
+++ include/openspace/util/setscene.h
@@ -1,4 +1,26 @@
-
+/*****************************************************************************************
+*                                                                                       *
+* OpenSpace                                                                             *
+*                                                                                       *
+* Copyright (c) 2014-2016                                                               *
+*                                                                                       *
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this  *
+* software and associated documentation files (the "Software"), to deal in the Software *
+* without restriction, including without limitation the rights to use, copy, modify,    *
+* merge, publish, distribute, sublicense, and/or sell copies of the Software, and to    *
+* permit persons to whom the Software is furnished to do so, subject to the following   *
+* conditions:                                                                           *
+*                                                                                       *
+* The above copyright notice and this permission notice shall be included in all copies *
+* or substantial portions of the Software.                                              *
+*                                                                                       *
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,   *
+* INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A         *
+* PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT    *
+* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF  *
+* CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE  *
+* OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                                         *
+****************************************************************************************/
 
 #ifndef __SETSCENE_H__
 #define __SETSCENE_H__
@@ -13,16 +35,12 @@
 #include <iostream>
 
 namespace openspace {
-
-
-
     std::string setScene(Scene* scene, Camera* camera, std::string _nameOfScene);
 
-    const glm::mat4 setNewViewMatrix(std::string, SceneGraphNode* target, Scene*);
+    const glm::mat4 setNewViewMatrix(const std::string & cameraParent, SceneGraphNode* target, Scene*);
 
-    glm::vec3 pathCollector(std::vector<SceneGraphNode*> path, std::string commonParent, bool inverse);
+    glm::vec3 pathCollector(const std::vector<SceneGraphNode*> & path, const std::string & commonParent, const bool inverse);
 
-    std::vector<SceneGraphNode*> pathTo(std::string nameOfScene, Scene* scene);
     void setRelativeOrigin(Camera* camera, Scene* scene);
 
     void newCameraOrigin(std::vector<SceneGraphNode*> commonParentPath, std::string commonParent, Camera* camera, Scene* scene);
@@ -31,7 +49,7 @@ namespace openspace {
 
     std::string commonParent(std::vector<SceneGraphNode*> t1, std::vector<SceneGraphNode*> t2);
 
-    SceneGraphNode* findCommonParentNode(std::string firstPath, std::string secondPath, Scene* scene);
+    SceneGraphNode* findCommonParentNode(const std::string & firstPath, const std::string & secondPath, Scene* scene);
 
     std::vector<SceneGraphNode*> pathTo(SceneGraphNode* node);
     
